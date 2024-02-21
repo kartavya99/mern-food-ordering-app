d0b9ca076c2e32daa548f9d5355c3a87fa3e1753
kartavya <kartavyas@gmail.com>
2024-02-21 20:06:22 +1100

    update user profil functionality on backend & frontend

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { access } from "fs";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -38,5 +40,56 @@ export const useCreateMyUser = () => {
     isLoading,
     isError,
     isSuccess,
+  };
+};
+
+type UpdateMyUserRequest = {
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
+export const useUpdateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateMyUserRequest = async (formData: UpdateMyUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutateAsync: updateUser,
+    isLoading,
+    isSuccess,
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+
+  if (isSuccess) {
+    toast.success("User profile successfully");
+  }
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
+
+  return {
+    updateUser,
+    isLoading,
   };
 };
