17e1567e26daf84c42f25e1cbde15fd25b6570f2
kartavya <kartavyas@gmail.com>
2024-03-05 15:03:56 +1100

    checkout feature added

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -2,8 +2,18 @@ import { useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoadingButton from "./LoadingButton";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import UserProfileForm, {
+  UserFormData,
+} from "@/forms/user-profile-form/UserProfileForm";
+import { useGetMyUser } from "@/api/MyUserApi";
 
-const CheckoutButton = () => {
+type Props = {
+  onCheckout: (userFormDate: UserFormData) => void;
+  disabled: boolean;
+};
+
+const CheckoutButton = ({ onCheckout, disabled }: Props) => {
   const {
     isAuthenticated,
     isLoading: isAuthLoading,
@@ -11,6 +21,8 @@ const CheckoutButton = () => {
   } = useAuth0();
 
   const { pathname } = useLocation();
+
+  const { currentUser, isLoading: isGetUserLoading } = useGetMyUser();
 
   // pathname = /detail/:restaurantId
   const onLogin = async () => {
@@ -24,15 +36,33 @@ const CheckoutButton = () => {
   if (!isAuthenticated) {
     return (
       <Button onClick={onLogin} className="bg-orange-500 flex-1">
-        {" "}
         Log in to check out
       </Button>
     );
   }
 
-  if (isAuthLoading) {
+  if (isAuthLoading || !currentUser) {
     return <LoadingButton />;
   }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button disabled={disabled} className="bg-orange-500 flex-1">
+          Go to checkout
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[425px] md:min-w-[700px] bg-gray-50">
+        <UserProfileForm
+          currentUser={currentUser}
+          onSave={onCheckout}
+          isLoading={isGetUserLoading}
+          title="Confirm Delivery Details"
+          buttonText="Continue to payment"
+        />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CheckoutButton;
