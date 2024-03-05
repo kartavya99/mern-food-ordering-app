6e76fd155cbe2366ed398b6012aee3c2d603e3f6
kartavya <kartavyas@gmail.com>
2024-03-05 14:40:40 +1100

    cartItem feature with redirect

--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -6,6 +6,15 @@ import RestaurantController from "../controllers/RestaurantController";
 const router = express.Router();
 
 router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId parameter must not be empty"),
+  RestaurantController.getRestaurant
+);
+router.get(
   "/search/:city",
   param("city")
     .isString()
---
+++ frontend/src/components/CheckoutButton.tsx
@@ -0,0 +1,38 @@
+import { useLocation } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoadingButton from "./LoadingButton";
+
+const CheckoutButton = () => {
+  const {
+    isAuthenticated,
+    isLoading: isAuthLoading,
+    loginWithRedirect,
+  } = useAuth0();
+
+  const { pathname } = useLocation();
+
+  // pathname = /detail/:restaurantId
+  const onLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: pathname,
+      },
+    });
+  };
+
+  if (!isAuthenticated) {
+    return (
+      <Button onClick={onLogin} className="bg-orange-500 flex-1">
+        {" "}
+        Log in to check out
+      </Button>
+    );
+  }
+
+  if (isAuthLoading) {
+    return <LoadingButton />;
+  }
+};
+
+export default CheckoutButton;
